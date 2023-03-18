5259e017422586e2ea9ca9bc7a7d10012f8377e2
nute11a <87134443+seungha164@users.noreply.github.com>
2023-03-19 00:10:27 +0900

    :sparkles: 로그인(및 자동 로그인 구현)
    - 로그인 화면 구성
    - 로그인 API 연동
    - securesotorage 이용 - 자동 로그인

--- lib/screens/BaseWidget.dart
+++ lib/screens/BaseWidget.dart
@@ -27,35 +27,38 @@ class _BaseWidget extends State<BaseWidget>{
       length: 3,
       initialIndex: 1, // 가운데에 있는 홈버튼을 기본값으로 설정
       // vsync: this,  나중에 다른 페이지 연결했을 때 사용
-      child: Scaffold(
-          body: SafeArea(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home, size: 18,),
-                  label: '홈',
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon( Icons.camera, size: 18,),
-                  label: 'cctv'
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person_outline, size: 18,),
-                  label: '일기'
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person_outline, size: 18,),
-                  label: '마이 자취'
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            unselectedItemColor: Colors.grey,
-            selectedItemColor: const Color(0xfffa625f),
-            onTap: _onItemTapped,
-          )
-      ),
+      child: WillPopScope(
+       onWillPop: () async => false,
+       child: Scaffold(
+           body: SafeArea(
+             child: _widgetOptions.elementAt(_selectedIndex),
+           ),
+           bottomNavigationBar: BottomNavigationBar(
+             items: const [
+               BottomNavigationBarItem(
+                 icon: Icon(Icons.home, size: 18,),
+                 label: '홈',
+               ),
+               BottomNavigationBarItem(
+                   icon: Icon( Icons.camera, size: 18,),
+                   label: 'cctv'
+               ),
+               BottomNavigationBarItem(
+                   icon: Icon(Icons.person_outline, size: 18,),
+                   label: '일기'
+               ),
+               BottomNavigationBarItem(
+                   icon: Icon(Icons.person_outline, size: 18,),
+                   label: '마이 자취'
+               ),
+             ],
+             currentIndex: _selectedIndex,
+             unselectedItemColor: Colors.grey,
+             selectedItemColor: const Color(0xfffa625f),
+             onTap: _onItemTapped,
+           )
+       ),
+      )
     );
   }
 
